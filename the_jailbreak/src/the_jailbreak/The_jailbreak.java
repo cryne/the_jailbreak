89760b6ebd9e47095f6a38841bc76c29c792f7b4
WilmerFloresMolina <wilmerflores-tux2@unitec.edu>
2016-11-18 20:39:45 -0600

    ya funciona el laberinto aletorio

--- the_jailbreak/src/the_jailbreak/The_jailbreak.java
+++ the_jailbreak/src/the_jailbreak/The_jailbreak.java
@@ -18,7 +18,6 @@ public class The_jailbreak {
      */
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        
                 String matriz[][] = new String[40][40];
 
         for (int i = 0; i < 40; i++) {
@@ -27,15 +26,20 @@ public class The_jailbreak {
             }
 
         }
-        Random rnd = new Random();
-        
-        for (int i = 0; i < 10; i++) {
-            System.out.println(rnd.nextInt(3));
+        for (int i = 0; i < 40; i++) {
+            matriz[i][0]="▓";
+           // matriz[i][39]="▓";
+            matriz[0][i]="▓";
+           // matriz[39][i]="▓";
+            
+            
         }
-        int num = rnd.nextInt(9);
+        Random rnd = new Random();
+
+        int num = rnd.nextInt(19)+1;
         matriz[num][0] = "E";
-        int num2 = rnd.nextInt(9);
-        matriz[num2][39] = "S";
+        int num2 = rnd.nextInt(39);
+      //  matriz[num2][39] = "S";
         System.out.println(num);
 
         /* for (int i = 0; i < 5; i++) {
@@ -47,79 +51,109 @@ public class The_jailbreak {
         }*/
         int x = 0;
         int y = 0;
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        
+        for (int i = 0; i < 40; i++) {
+            for (int j = 0; j < 40; j++) {
                 if (matriz[i][j].equals("E")) {
                     x = i;
                     y = j;
                 }
             }
         }
-
-        for (int r = 0;r<200;r++) {
+        int cont = 1;
+        for (int r = 0; r < 200; r++) {
             int temp = rnd.nextInt(3);
-            System.out.println(temp);
-            int tempx = 0;
-            int tempy = 0;
-            if (temp==2){
-                int h= y+1;
-                if (y<39 && 0<=y) {
-                    if (matriz[x][h].equals (" ")) {
-                     matriz[x][h]="-";
-                     y=y+1;
+          //  System.out.println(temp);
+            
+            if (cont==1) {
+              matriz[x][y+1]="-";
+              y=y+1;
+              cont++;
+            }else{
+            
+                if (temp == 2) {
+                    int h = y + 1;
+                    if (y < 39 && 0 <= y) {
+                        if (matriz[x][h].equals(" ")) {
+                            if (h == 39) {
+                                matriz[x][h] = "S";
+                            } else {
+                                matriz[x][h] = "-";
+                                y = y + 1;
+                            }
+                        }
                     }
-                }
-                if(y==39){
-                    break;
-                }
-               
-            }
-            
-            if (temp==0){
-                int h= x+1;
-                if (x<39 && 0<=x) {
-                    if (matriz[x][h].equals (" ")) {
-                       matriz[h][y]="-";
-                     x=x+1; 
+                    if (y == 39) {
+                        break;
                     }
-                     
+
                 }
-                
-               
-            }
-            //if
-            if (temp==3){
-                int h= x-1;
-                if (x<39 && 0<x) {
-                    if (matriz[x][h].equals (" ")) {
-                     matriz[h][y]="-";
-                     x=x-1;
+
+                if (temp == 0) {
+                    int h = x + 1;
+                    if (x < 38 && 0 <= x) {
+                        if (matriz[x][h].equals(" ")) {
+                            matriz[h][y] = "-";
+                            x = x + 1;
+                        }
+
                     }
+
                 }
-                
-               
+                //if
+                if (temp == 3) {
+                    int h = x - 1;
+                    if (x < 38 && 0 < x) {
+                        if (matriz[x][h].equals(" ")) {
+                            matriz[h][y] = "-";
+                            x = x - 1;
+                        }
+                    }
+
+                }
+
             }
-         
-        //  System.out.println("-------");
-            System.out.println("x0=" +tempx);
-            System.out.println("y0="+tempy);
-            
-           // System.out.println("");
-            
-            int numx = 0;
-            int numy = 0;
-        
-            
+                int numx = 0;
+                int numy = 0;
 
         }//fin while
-
+        for (int i = 0; i < 1300; i++) {
+            num = rnd.nextInt(39);
+        
+        num2 = rnd.nextInt(39);
+            if (matriz[num][num2].equals(" ")) {
+                matriz[num][num2]="-";
+            }
+            
+        }
+        for (int i = 0; i < 40; i++) {
+            for (int j = 0; j < 40; j++) {
+                if (matriz[i][j].equals(" ")) {
+                    matriz[i][j]="▓";
+                }
+                
+            }
+        }
+         for (int i = 0; i < 40; i++) {
+            for (int j = 0; j < 40; j++) {
+                if (matriz[i][j].equals("-")) {
+                    matriz[i][j]="░";
+                }
+                
+            }
+        }
+         
+         
+        
         for (int i = 0; i < 40; i++) {
             System.out.println("");
             for (int j = 0; j < 40; j++) {
-                System.out.print("[" + matriz[i][j] + "] ");
+                System.out.print("" + matriz[i][j] + "");
             }
 
         }//fin for
+        
+ 
 
     }
     
