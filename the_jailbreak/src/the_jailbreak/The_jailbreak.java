5e9f23b062402d275b53e3202b6e8a5e4a234f73
WilmerFloresMolina <wilmerflores-tux2@unitec.edu>
2016-11-18 18:29:34 -0600

    prueba 3

--- the_jailbreak/src/the_jailbreak/The_jailbreak.java
+++ the_jailbreak/src/the_jailbreak/The_jailbreak.java
@@ -5,6 +5,8 @@
  */
 package the_jailbreak;
 
+import java.util.Random;
+
 /**
  *
  * @author Romero
@@ -16,6 +18,109 @@ public class The_jailbreak {
      */
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        
+                String matriz[][] = new String[40][40];
+
+        for (int i = 0; i < 40; i++) {
+            for (int j = 0; j < 40; j++) {
+                matriz[i][j] = " ";
+            }
+
+        }
+        Random rnd = new Random();
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(rnd.nextInt(3));
+        }
+        int num = rnd.nextInt(9);
+        matriz[num][0] = "E";
+        int num2 = rnd.nextInt(9);
+        matriz[num2][39] = "S";
+        System.out.println(num);
+
+        /* for (int i = 0; i < 5; i++) {
+            System.out.println("");
+            for (int j = 0; j < 5; j++) {
+                System.out.print("[" + matriz[i][j] + "] ");
+            }
+
+        }*/
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (matriz[i][j].equals("E")) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        for (int r = 0;r<200;r++) {
+            int temp = rnd.nextInt(3);
+            System.out.println(temp);
+            int tempx = 0;
+            int tempy = 0;
+            if (temp==2){
+                int h= y+1;
+                if (y<39 && 0<=y) {
+                    if (matriz[x][h].equals (" ")) {
+                     matriz[x][h]="-";
+                     y=y+1;
+                    }
+                }
+                if(y==39){
+                    break;
+                }
+               
+            }
+            
+            if (temp==0){
+                int h= x+1;
+                if (x<39 && 0<=x) {
+                    if (matriz[x][h].equals (" ")) {
+                       matriz[h][y]="-";
+                     x=x+1; 
+                    }
+                     
+                }
+                
+               
+            }
+            //if
+            if (temp==3){
+                int h= x-1;
+                if (x<39 && 0<x) {
+                    if (matriz[x][h].equals (" ")) {
+                     matriz[h][y]="-";
+                     x=x-1;
+                    }
+                }
+                
+               
+            }
+         
+        //  System.out.println("-------");
+            System.out.println("x0=" +tempx);
+            System.out.println("y0="+tempy);
+            
+           // System.out.println("");
+            
+            int numx = 0;
+            int numy = 0;
+        
+            
+
+        }//fin while
+
+        for (int i = 0; i < 40; i++) {
+            System.out.println("");
+            for (int j = 0; j < 40; j++) {
+                System.out.print("[" + matriz[i][j] + "] ");
+            }
+
+        }//fin for
+
     }
     
 }
